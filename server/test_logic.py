68a2dea132f753f337d940aae190cdf42fc01feb
xephyz <xephyzone@gmail.com>
2020-04-15 17:25:27 +0200

    Smol fix for tests :ok_hand:

--- server/test_logic.py
+++ server/test_logic.py
@@ -4,15 +4,22 @@ from logic import Card, Klondike, build_game, draw, move
 
 class CardTest(unittest.TestCase):
 
-    def test_repr(self):
+    def test_card_repr(self):
         card = Card(1, 'h')
-        self.assertEqual(str(card), '| 1h|')
+        self.assertEqual(str(card), ' 1h')
 
 
 class KlondikeTest(unittest.TestCase):
 
-    def draw_test(self):
+    def test_build_game(self):
+        pass
+
+    def test_draw(self):
         stock = [Card(1, 'h'), Card(2, 'h'), Card(3, 'h'), Card(4, 'h')]
         pile = [Card(5, 'h')]
-        result = draw(stock, pile)
-        self.assertEqual((stock[:1], pile.append(stock[1:])), result)
+        _stock, _pile = draw(stock, pile)
+        self.assertEqual((len(stock), len(pile)), (len(_pile), len(_stock)))
+
+
+if __name__ == "__main__":
+    unittest.main()
