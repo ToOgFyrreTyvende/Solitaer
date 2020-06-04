fde3fff8baba99b725abec4e19a8da10f5387af8
Anders Michaelsen <37267649+Xephyz@users.noreply.github.com>
2020-06-04 14:49:49 +0200

    Remade ai tests with pytest
    Turned the old unittests into much simpler versions in pytest, which also allow us to use one simple test with several different inputs in a smart way using `parametrize` from pytest! :ok_hand:

--- server/test_ai.py
+++ server/test_ai.py
@@ -1,27 +1,64 @@
-import unittest
 from copy import deepcopy
+from typing import Union, Tuple
+
+import pytest
 
 from ai import new_find_move
-from logic import build_game, draw, move
-
-game = build_game(False)
+from logic import build_game, draw, move, Klondike
 
 
-class Test(unittest.TestCase):
-    def test_new_find_move_new_game(self):
-        self.assertEqual(new_find_move(game), -1)
+def build_test_games() -> Tuple[Klondike, Klondike, Klondike, Klondike]:
+    # No games are shuffled, to avoid random stuff
+    game2 = build_game(False)
+    game2.stock, game2.pile = draw(game2.stock, game2.pile)
 
-    def test_new_find_move_tableau_to_foundation(self):
-        _game = deepcopy(game)
-        _game.stock, _game.pile = draw(_game.stock, _game.pile)
-        self.assertTupleEqual(new_find_move(_game), (2, 1, 2, 1))
+    game4 = deepcopy(game2)
+    game4.tableaus[1], game4.tableaus[2] = move(1, game4.tableaus[1], game4.tableaus[2])
+    game4.tableaus[3], game4.tableaus[4] = move(1, game4.tableaus[3], game4.tableaus[4])
+    game4.tableaus[5], game4.tableaus[6] = move(1, game4.tableaus[5], game4.tableaus[6])
+    game4.tableaus[5], game4.tableaus[6] = move(1, game4.tableaus[5], game4.tableaus[6])
 
-    # def test_new_find_move_big_boi(self):
-    #     _game = deepcopy(game)
-    #     self.assertEqual(new_find_move(_game), -1)
-    #     _game.stock, _game.pile = draw(_game.stock, _game.pile)
-    #     self.assertTupleEqual(new_find_move(_game), (2, 1, 2, 1))
+    game3 = deepcopy(game4)
+    game3.pile, game3.tableaus[2] = move(1, game3.pile, game3.tableaus[2])
+    game3.pile, game3.tableaus[0] = move(1, game3.pile, game3.tableaus[0])
+    game3.pile, game3.tableaus[1] = move(1, game3.pile, game3.tableaus[1])
+    game3.stock, game3.pile = draw(game3.stock, game3.pile)
+    game3.pile, game3.tableaus[2] = move(1, game3.pile, game3.tableaus[2])
+    game3.stock, game3.pile = draw(game3.stock, game3.pile)
+    game3.stock, game3.pile = draw(game3.stock, game3.pile)
+    game3.pile, game3.tableaus[2] = move(1, game3.pile, game3.tableaus[2])
+    game3.stock, game3.pile = draw(game3.stock, game3.pile)
+    game3.stock, game3.pile = draw(game3.stock, game3.pile)
+    game3.stock, game3.pile = draw(game3.stock, game3.pile)
+
+    game1 = deepcopy(game3)
+    game1.pile, game1.foundations[0] = move(1, game1.pile, game1.foundations[0])
+    game1.stock, game1.pile = draw(game1.stock, game1.pile)
+    game1.pile, game1.foundations[1] = move(1, game1.pile, game1.foundations[1])
+    game1.pile, game1.foundations[2] = move(1, game1.pile, game1.foundations[2])
+    game1.pile, game1.foundations[3] = move(1, game1.pile, game1.foundations[3])
+    game1.pile, game1.foundations[1] = move(1, game1.pile, game1.foundations[1])
+    game1.pile, game1.foundations[2] = move(1, game1.pile, game1.foundations[2])
+    game1.pile, game1.foundations[3] = move(1, game1.pile, game1.foundations[3])
+    game1.pile, game1.foundations[0] = move(1, game1.pile, game1.foundations[0])
+    game1.pile, game1.foundations[1] = move(1, game1.pile, game1.foundations[1])
+
+    return game2, game4, game3, game1
+
+
+game_after_draw, g_move4, g_move3, g_move1 = build_test_games()
+
+
+@pytest.mark.parametrize('game, expected', [
+    (build_game(), (-1,)),
+    (game_after_draw, (2, 1, 2, 1)),
+    (g_move4, (4, 2)),
+    (g_move3, (3, 0)),
+    (g_move1, (1, 2, 1))
+])
+def test_new_find_move(game: Klondike, expected: Union[int, Tuple[int, int], Tuple[int, int, int], Tuple[int, int, int, int]]):
+    assert new_find_move(game) == expected
 
 
 if __name__ == "__main__":
-    unittest.main()
+    pytest.main()
