650324499174571080ab610fb8d5f3a814a5637b
Ahmad Sattar <thehabbos007@gmail.com>
2020-06-15 16:37:52 +0200

    cool

--- server/card_detector/helpers.py
+++ server/card_detector/helpers.py
@@ -6,32 +6,40 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 
 BG_THRESHOLD = 130
 
-kernel = np.ones((5,5), np.uint8) 
+kernel = np.ones((5, 5), np.uint8)
+
 
 def get_background_reference(gray_image):
-    image_w, image_h  = np.shape(gray_image)[:2]
+    image_w, image_h = np.shape(gray_image)[:2]
     # Select random pixel at the bottom left as background reference point
-    background_pixel =  gray_image[int(image_w/100)][int(image_h/100)]
+    background_pixel = gray_image[int(image_w/100)][int(image_h/100)]
     return background_pixel + BG_THRESHOLD
 
-def threshold_image(image):
-    grayed  = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(grayed, (1,1), 1000)
 
-    thresh_level   = get_background_reference(grayed)
-    _, thresholded = cv2.threshold(blurred, thresh_level, 255, cv2.THRESH_BINARY)
-    
+def threshold_image(image):
+    grayed = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(grayed, (1, 1), 1000)
+
+    thresh_level = get_background_reference(grayed)
+    _, thresholded = cv2.threshold(
+        blurred, thresh_level, 255, cv2.THRESH_BINARY)
+    thresholded = cv2.dilate(thresholded, kernel)
+
     return thresholded
+
 
 def find_number_suit(thresholded_image, img):
     #thresholded_image = cv2.erode(thresholded_image, kernel, iterations=1)
-    cnts, hier = cv2.findContours(thresholded_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts, hier = cv2.findContours(
+        thresholded_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     #numcards = len(cnts)-1
     #contours = sorted(cnts, key=cv2.contourArea, reverse=True)[:numcards]
     rects = []
+    img_slices = []
     contor_img = None
+    cutoff = img.shape[0]*img.shape[1]*0.009
     for c in cnts:
-        if cv2.contourArea(c) < 7000:
+        if cv2.contourArea(c) < cutoff:
             continue
         # Idea: Create approximation trapezoid
         # Use trapezoid corners as homography or warpperspective
@@ -39,10 +47,44 @@ def find_number_suit(thresholded_image, img):
         box = cv2.minAreaRect(c)
         box = cv2.boxPoints(box)
         box = np.array(box, dtype="int")
+        img_slices.append(warp_bboxes(img, box))
         cv2.drawContours(img, [box], -1, (0, 255, 0), 2)
         rects.append(box)
-        #h, status = cv2.findHomography(pts_src, pts_dst)
-        #cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
 
+        #h, status = cv2.findHomography(pts_src, pts_dst)
+        # cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
     #cv2.imshow('convex hull',contor_img)
-    return rects
+    return img_slices
+
+
+def warp_bboxes(img, box):
+    rotated_contour = np.zeros((4, 2), dtype="float32")
+    s = box.sum(axis=1)
+    # Top left has smallest sum
+    rotated_contour[0] = box[np.argmin(s)]
+    # Bottom right has largest sum
+    rotated_contour[2] = box[np.argmax(s)]
+    diff = np.diff(box, axis=1)
+    # Top right has smallest diff
+    rotated_contour[1] = box[np.argmin(diff)]
+    # Bottom left has largest diff
+    rotated_contour[3] = box[np.argmax(diff)]
+
+    [tl, tr, br, bl] = rotated_contour
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+
+    maxWidth = max(int(widthA), int(widthB))
+    maxHeight = max(int(heightA), int(heightB))
+
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+
+    M = cv2.getPerspectiveTransform(rotated_contour, dst)
+    return cv2.warpPerspective(img, M, (maxWidth, maxHeight))
--- server/card_detector/yolodetector.py
+++ server/card_detector/yolodetector.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-from card_detector.helpers import *
+from helpers import *
 from numpy.linalg import norm
 import os
 
@@ -23,6 +23,7 @@ with open(names, "r") as f:
 layer_names = nnet.getLayerNames()
 output_layers = [layer_names[i[0] - 1] for i in nnet.getUnconnectedOutLayers()]
 
+
 def detect_cards(outputs, real_w, real_h):
     bounding_boxes = []
     confidence_scores = []
@@ -36,12 +37,13 @@ def detect_cards(outputs, real_w, real_h):
             # The remaining elements of this array will have a value from 0.0 - 1.0
             scores = detection[5:]
             # The class_id can be found from the INDEX of the highest value of these scores (the scores of each class)
-            detection_class = np.argmax(scores) # argmax returns index of highest value
+            # argmax returns index of highest value
+            detection_class = np.argmax(scores)
             # After this is found, the confidence score can be found at that index
             confidence = scores[detection_class]
             # We want to reject predictions with a low confidence, here we choose 40% percent confidence as the cutoff
             # found through trial and error
-                # Only unique identifications are wanted here, so this is checked against a set
+            # Only unique identifications are wanted here, so this is checked against a set
             if confidence > CONFIDENCE_CUTOFF and detection_class not in detected_classes_set:
                 detected_classes_set.add(detection_class)
                 # The detection position can now be found
@@ -61,54 +63,79 @@ def detect_cards(outputs, real_w, real_h):
 
     return (bounding_boxes, confidence_scores, datected_classes)
 
+
 def clean_detections(detections):
     # We need to run the data through non maximum suppression.
     # This removes boxes around the same detection.
     (bounding_boxes, confidence_scores, detected_classes) = detections
     if not bounding_boxes:
         return []
-        
-    indices = cv2.dnn.NMSBoxes(bounding_boxes, confidence_scores, CONFIDENCE_CUTOFF, 0.4).flatten()
 
-    # This will result in array of form 
+    indices = cv2.dnn.NMSBoxes(
+        bounding_boxes, confidence_scores, CONFIDENCE_CUTOFF, 0.4).flatten()
+
+    # This will result in array of form
     # [(class_id, confidence, [x,y,w,h])
     return [(detected_classes[i], confidence_scores[i], bounding_boxes[i]) for i in indices]
-    
+
 
 def draw_cards(img, detections):
     for class_id, confidence, pos in detections:
         [x, y, w, h] = pos
         label = str(CLASS_IDS[class_id])
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(img, label, (x, y + 30), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 3)
+        cv2.putText(img, label, (x, y + 30),
+                    cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 3)
 
 
 def infer_from_image(raw_img):
     # Scale image to be only 40% of width and height
-    img = cv2.resize(raw_img, None, fx=0.4, fy=0.4)
+    #img = cv2.resize(raw_img, None, fx=1, fy=1)
     height, width, channels = raw_img.shape
+    w_scale = 1
+    h_scale = 1
+    if height > 608:
+        h_scale = (height-(height-605))/height
+    if width > 608:
+        w_scale = (width-(width-605))/width
+
+    if w_scale != 1 or h_scale != 1:
+        raw_img = cv2.resize(raw_img, None, fx=w_scale, fy=h_scale)
+
+    height, width, channels = raw_img.shape
+
+    top_bot = int((608-height)/2)
+    left_right = int((608-width)/2)
+
+    image = cv2.copyMakeBorder(
+        raw_img, top_bot, top_bot, left_right, left_right, cv2.BORDER_CONSTANT)
 
     # Detecting the objects
     # https://docs.opencv.org/master/d6/d0f/group__dnn.html#ga29f34df9376379a603acd8df581ac8d7
     # The first argument after img is the scale factor, then size, mean and swrapRB
     # Scale factor is a multiplyer to scale. It is suggested to let this be 1/255, same as 0.00392
-        # https://www.learnopencv.com/deep-learning-based-object-detection-using-yolov3-with-opencv-python-c/
+    # https://www.learnopencv.com/deep-learning-based-object-detection-using-yolov3-with-opencv-python-c/
     # Size given as the target resolution, YOLO takes 416x416 images
     # Mean is scalar values subtracted from channels. We do not change any color values.
     # SwapRB indicates swapping first and last channels in 3 channel images
-    blob = cv2.dnn.blobFromImage(img, 1/255, (608, 608), (0, 0, 0), True, crop=False)
+    blob = cv2.dnn.blobFromImage(
+        image, 1/255, (608, 608), (0, 0, 0), True, crop=False)
     # Load binary image data into our neural network and infer
     nnet.setInput(blob)
     outputs = nnet.forward(output_layers)
-    detections = detect_cards(outputs, width, height)
+    detections = detect_cards(outputs, 608, 608)
     clean_detect = clean_detections(detections)
+    print(clean_detect)
     if DEBUG:
-        draw_cards(raw_img, clean_detect)
+        draw_cards(image, clean_detect)
+        cv2.imshow("bombie", image)
+        cv2.waitKey(0)
     return clean_detect
 
 
-def area(a, b, c) :
-    return 0.5 * norm( np.cross( b-a, c-a ) )
+def area(a, b, c):
+    return 0.5 * norm(np.cross(b-a, c-a))
+
 
 def create_stacks(bboxes, detections):
     pile = None
@@ -152,29 +179,33 @@ def create_stacks(bboxes, detections):
                 D-------------C
                 If the APD + DPC + CPB + PBA > ABC + CDA, then the point P is not within the rectangle ABCD
                 """
-                area_check = area(A, P, D) + area(D, P, C) + area(C, P, B) + area(P, B, A)
+                area_check = area(A, P, D) + area(D, P, C) + \
+                    area(C, P, B) + area(P, B, A)
                 if area_check == rect_area:
                     cards.append(str(CLASS_IDS[class_id]))
         else:
             for class_id, confidence, pos in detections:
                 [cx, cy, cw, ch] = pos
                 P = [cx + cw/2, cy + ch/2]
-                area_check = area(A, P, D) + area(D, P, C) + area(C, P, B) + area(P, B, A)
+                area_check = area(A, P, D) + area(D, P, C) + \
+                    area(C, P, B) + area(P, B, A)
                 if area_check == rect_area:
                     if i == 1:
                         pile = str(CLASS_IDS[class_id])
                     else:
                         foundations.append(str(CLASS_IDS[class_id]))
 
-
         tableaus.append((A[0], cards))
     return (foundations[0], [foundations[1]], [x[1] for x in sorted(tableaus, key=lambda x: x[0])])
 
+
 def extract_cards_from_image(img):
-    detections = infer_from_image(img)
+    #detections = infer_from_image(img)
     processed_img = threshold_image(img)
-    bboxes = find_number_suit(processed_img, img)
-    return detections
+    img_slices = find_number_suit(processed_img, img)
+    [infer_from_image(x) for x in img_slices]
+    return "oke"
+
 
 def get_card_classes(detections):
     res = []
@@ -185,8 +216,7 @@ def get_card_classes(detections):
 
 
 if __name__ == "__main__":
-    img = cv2.resize(cv2.imread("cards4.jpg"), (1280,720))
+    img = cv2.resize(cv2.imread("cards4.jpg"), (1280, 720))
     print(extract_cards_from_image(img))
     cv2.imshow("Image", img)
     cv2.waitKey(0)
-    
