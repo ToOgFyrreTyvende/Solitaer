d85491db3b178f3edd436d675971647da88d1db5
Ahmad Sattar <thehabbos007@gmail.com>
2020-06-17 12:13:06 +0200

    New diagram

--- client/src/components/IntroModal.vue
+++ client/src/components/IntroModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-modal ref="modal" id="modal" size="xl" title="Introduction" hide-backdrop>
+  <b-modal ref="intro_modal" id="intro_modal" size="xl" title="Introduction" hide-backdrop>
     Welcome to our Solitaire hinting app!
     In order to use this program, we have a couple of requirements:
     <ul>
@@ -7,36 +7,52 @@
       <li>A dark background for the picture.</li>
       <li>Please allow time after taking a picture for the results to come in!</li>
     </ul>And please make use of the guide lines to seperate the pile, foundations and tableaus!
-    <template
-      v-slot:modal-footer="{ ok, hide }"
-    >
-      <b-button size="lg" variant="outline-secondary" @click="hide()">Skip tour</b-button>
+    <p>
+      Each new hint requires you to take a new picture of the updated playing board.
+      The hints come back to the website in the following form:
+    </p>
+    <card-diagram id="example_diagram" kind="tableau" :move="card_diagram_data"></card-diagram>
+    <p>You can take a tour of the UI by clicking "Start tour", otherwise if you're ready to play, press skip!</p>
+
+    <template v-slot:modal-footer="{ ok, hide }">
+      <b-button size="lg" variant="outline-secondary" @click="hide()">Skip</b-button>
       <b-button size="lg" variant="primary" @click="start_tour(ok)">Start tour</b-button>
     </template>
   </b-modal>
 </template>
 
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+#example_diagram {
+  margin: 25px 0;
 }
 </style>
 
 
 <script>
+import CardDiagram from "./CardDiagram.vue";
+
 export default {
   name: "IntroModal",
-  data: {
-    callback: null
+  components: {
+    "card-diagram": CardDiagram
+  },
+  data() {
+    return {
+      callback: null,
+      card_diagram_data: { from: "Qs", to: "Kh" }
+    };
   },
   methods: {
     show(callback) {
       this.callback = callback;
-      this.$refs.modal.show();
+      this.$refs.intro_modal.show();
     },
     start_tour(ok) {
       ok();
       this.callback();
+    },
+    hint_img() {
+      return Images.hint;
     }
   }
 };
