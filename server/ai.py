c38b384810ddbe6c1a6cb145ad7d168636cd3ab5
Anders Michaelsen <37267649+Xephyz@users.noreply.github.com>
2020-06-18 19:56:03 +0200

    Removed old find move
    Finally...

--- server/ai.py
+++ server/ai.py
@@ -102,55 +102,6 @@ def find_move_wrapper(g: Klondike) -> Dict[str, Union[str, Dict[str, Union[str, 
     return response
 
 
-def find_move(g: Klondike):
-    moves = 0
-    # pile
-    if len(g.pile) == 0 and len(g.stock) != 0:
-        print("Draw")
-        g.stock, g.pile = draw(g.stock, g.pile)
-        # print_game(g)
-    if len(g.pile) != 0:
-        for f in range(4):  # looks through different foundations 
-            if check_move(g.pile[-1], g.foundations[f], to_foundation = True):
-                g.pile, g.foundations[f] = move(1, g.pile, g.foundations[f])
-                return
-        for j in range(len(g.tableaus)):  # looks through different tableaus
-            # if len(g.tableaus[j]) == 0:
-            #       print("Skip " + str(j+1))
-            #       continue
-            if check_move(g.pile[-1], g.tableaus[j]):
-                g.pile, g.tableaus[j] = move(1, g.pile, g.tableaus[j])
-                print("Match in tableau " + str(j+1) + " Move " + str(1) + " 0 " + str(j+1))
-                # print_game(g)
-                moves += 1
-                return
-
-    # tableaus
-    for i in range(7):
-        if len(g.tableaus[i]) == 0:
-            #print(" Skip" + str(i+1))
-            continue
-        for f in range(4):  # looks through different foundations 
-            if check_move(g.tableaus[i][-1], g.foundations[f], to_foundation = True):
-                g.tableaus[i], g.foundations[f] = move(1, g.tableaus[i], g.foundations[f])
-                return
-        for j in range(len(g.tableaus)):  # looks through different tableaus
-            # if len(g.tableaus[j]) == 0:
-            #    print("Skip " + str(j+1))
-            #    continue 
-            if g.tableaus[i][-get_nr_of_flipped(g.tableaus[i])] == g.tableaus[i][0] and g.tableaus[i][0].value == 13: continue
-            if check_move(g.tableaus[i][-get_nr_of_flipped(g.tableaus[i])], g.tableaus[j]):
-                g.tableaus[i], g.tableaus[j] = move(get_nr_of_flipped(g.tableaus[i]), g.tableaus[i], g.tableaus[j])
-                print("Match in tableau " + str(j+1) + " Move " + str(1) + " " + str(i+1) + " " + str(j+1))
-                # print_game(g)
-                moves += 1
-                return
-    if moves == 0:
-        print("Draw")
-        g.stock, g.pile = draw(g.stock, g.pile)
-        # print_game(g)
-
-
 def main():
     g = Klondike.new_game(False)
     print_game(g); print()
