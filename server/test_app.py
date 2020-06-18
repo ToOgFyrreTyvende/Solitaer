17e90867a7d11994bb677e41e47df2fe57b98e59
Anders Michaelsen <37267649+Xephyz@users.noreply.github.com>
2020-06-18 16:15:20 +0200

    Updated `test_find_move_wrapper` function
    It now uses the function implemented in app.py (`get_move_from_img`) to test that it works instead of implementing the same functionality and testing that :ok_hand:

--- server/test_app.py
+++ server/test_app.py
@@ -6,6 +6,7 @@ import numpy as np
 import pytest
 
 from ai import find_move_wrapper
+from app import get_move_from_img
 from card_detector.yolodetector import new_extract_cards_from_image
 from logic import Klondike, Card, print_game
 
@@ -19,18 +20,7 @@ images = [os.path.join(card_detector_dir, item) for item in images]
     (cv2.imread(images[0]), {'kind': 'MOVE', 'move': {'to': None, 'from': None}})
 ])
 def test_find_move_wrapper(img: np.ndarray, expected: Dict):
-    (img_pile, img_founds, img_tableaus) = new_extract_cards_from_image(img)
-    img_pile = img_pile[0]
-
-    game = Klondike()
-    game.pile = [Card.from_str(card) for card in img_pile]
-    game.foundations = [[Card.from_str(card) for card in lst] for lst in img_founds]
-    game.tableaus = [[Card.from_str(card) for card in lst] for lst in img_tableaus]
-    print_game(game)
-
-    next_move = find_move_wrapper(game)
-
-    assert next_move == expected
+    assert get_move_from_img(img) == expected
 
 
 if __name__ == '__main__':
