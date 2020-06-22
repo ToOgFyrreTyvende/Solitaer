1fe9c4be2e741e1d43c7bf939d0b0b421a65a5e3
Anders Michaelsen <37267649+Xephyz@users.noreply.github.com>
2020-06-22 17:34:01 +0200

    Update test_ai.py
    Updated expected values for ai tests to new format.
    Also cleaned up the test code a little to be more readable/understandable
    + other smol improvements

--- server/test_ai.py
+++ server/test_ai.py
@@ -1,10 +1,14 @@
 from copy import deepcopy
-from typing import Union, Tuple
+from typing import Union, Tuple, Dict, Optional
 
 import pytest
 
 from ai import MOVE_CODE, new_find_move, find_move_wrapper
 from logic import draw, move, Klondike, get_card_at, Card
+
+# Custom types
+Move = Union[int, Tuple[int, int], Tuple[int, int, int], Tuple[int, int, int, int]]
+WrappedMove = Dict[str, Union[str, Dict[str, Optional[str]]]]
 
 
 def build_test_games() -> Tuple[Klondike, Klondike, Klondike, Klondike]:
@@ -46,39 +50,47 @@ def build_test_games() -> Tuple[Klondike, Klondike, Klondike, Klondike]:
     return game2, game4, game3, game1
 
 
-game_after_draw, g_move4, g_move3, g_move1 = build_test_games()
+g_move_tt, g_move_pt, g_move_pf, g_move_tf = build_test_games()
 
-game5 = Klondike()
-game5.tableaus[0] = [Card.from_str('As')]
+g_ace_to_f = Klondike()
+g_ace_to_f.tableaus[0].append(Card.from_str('As'))
+
 
 @pytest.mark.parametrize('game, expected', [
     (Klondike.new_game(), (MOVE_CODE.DRAW,)),
-    (game_after_draw, (MOVE_CODE.T_TO_T, 1, 2, 1)),
-    (g_move4, (MOVE_CODE.P_TO_T, 2)),
-    (g_move3, (MOVE_CODE.P_TO_F, 0)),
-    (g_move1, (MOVE_CODE.T_TO_F, 2, 1)),
-    (game5, (MOVE_CODE.T_TO_F, 0, 0))
+    (g_move_tt, (MOVE_CODE.T_TO_T, 1, 2, 1)),
+    (g_move_pt, (MOVE_CODE.P_TO_T, 2)),
+    (g_move_pf, (MOVE_CODE.P_TO_F, 0)),
+    (g_move_tf, (MOVE_CODE.T_TO_F, 2, 1)),
+    (g_ace_to_f, (MOVE_CODE.T_TO_F, 0, 0))
 ])
-def test_new_find_move(game: Klondike, expected: Union[int, Tuple[int, int], Tuple[int, int, int], Tuple[int, int, int, int]]):
+def test_new_find_move(game: Klondike, expected: Move):
     assert new_find_move(game) == expected
 
 
-def wrapper_dict_builder(kind: str = 'MOVE', move_to: str = None, move_from: str = None):
+def wrapper_dict_builder(kind: str = 'MOVE', move_to: str = None, move_from: str = None) -> WrappedMove:
     return {'kind': kind, 'move': {'to': move_to, 'from': move_from}}
 
 
 @pytest.mark.parametrize('game, expected', [
     (Klondike.new_game(), wrapper_dict_builder(kind='DRAW')),
-    (game_after_draw, wrapper_dict_builder(move_from=get_card_at(game_after_draw.tableaus[1]),
-                                           move_to=get_card_at(game_after_draw.tableaus[2]))),
-    (g_move4, wrapper_dict_builder(move_from=get_card_at(g_move4.pile),
-                                   move_to=get_card_at(g_move4.tableaus[2]))),
-    (g_move3, wrapper_dict_builder(move_from=get_card_at(g_move3.pile),
-                                   move_to=get_card_at(g_move3.foundations[0]))),
-    (g_move1, wrapper_dict_builder(move_from=get_card_at(g_move1.tableaus[2]),
-                                   move_to=get_card_at(g_move1.foundations[1])))
+    (g_move_tt, wrapper_dict_builder(kind='TT',
+                                     move_from=get_card_at(g_move_tt.tableaus[1]),
+                                     move_to=get_card_at(g_move_tt.tableaus[2]))),
+    (g_move_pt, wrapper_dict_builder(kind='PT',
+                                     move_from=get_card_at(g_move_pt.pile),
+                                     move_to=get_card_at(g_move_pt.tableaus[2]))),
+    (g_move_pf, wrapper_dict_builder(kind='PF',
+                                     move_from=get_card_at(g_move_pf.pile),
+                                     move_to=get_card_at(g_move_pf.foundations[0]))),
+    (g_move_tf, wrapper_dict_builder(kind='TF',
+                                     move_from=get_card_at(g_move_tf.tableaus[2]),
+                                     move_to=get_card_at(g_move_tf.foundations[1]))),
+    (g_ace_to_f, wrapper_dict_builder(kind='TF',
+                                      move_from=get_card_at(g_ace_to_f.tableaus[0]),
+                                      move_to=get_card_at(g_ace_to_f.foundations[0])))
 ])
-def test_find_move_wrapper(game: Klondike, expected: Union[int, Tuple[int, int], Tuple[int, int, int], Tuple[int, int, int, int]]):
+def test_find_move_wrapper(game: Klondike, expected: WrappedMove):
     assert find_move_wrapper(game) == expected
 
 
